bd4b7e8b4a51f110f67653e95359fad1a4b7b56d
Alsa M P <alsa.mp@pitsolutions.com>
2019-08-26 16:29:06 +0530

    Initial commit

---
+++ Models/ShowData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApplication2.Models
+{
+    public class ShowData
+    {
+        public string Name { get; set; }
+    }
+}
